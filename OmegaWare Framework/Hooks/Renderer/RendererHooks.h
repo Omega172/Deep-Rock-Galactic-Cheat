fd5ce60475e302718a86dd59988ce2c8e8c5d273
Good Evening <114830237+GoodEveningFellOff@users.noreply.github.com>
2024-02-22 10:27:16 -0500

    Reverted changes made in the renderer

--- OmegaWare Framework/Hooks/Renderer/RendererHooks.h
+++ OmegaWare Framework/Hooks/Renderer/RendererHooks.h
@@ -1,37 +1,13 @@
 #pragma once
 #include "pch.h"
 
-#if FRAMEWORK_RENDER_D3D11
-#include "Hooks/Renderer/D3D11/D3D11Hooks.h"
-#endif
-
-#if FRAMEWORK_RENDER_D3D12
-#include "Hooks/Renderer/D3D12/D3D12Hooks.h"
-#endif
-
 class RendererHooks
 {
 private:
-	bool Initialized = false;
 
 public:
 
-	bool Setup()
-	{
-#if FRAMEWORK_RENDER_D3D11
-		Initialized = D3D11Setup();
-		return Initialized;
-#endif
+	bool Setup();
 
-		return false;
-	}
-
-	void Destroy()
-	{
-#if FRAMEWORK_RENDER_D3D11
-		D3D11Destroy();
-#endif
-
-		return;
-	}
+	void Destroy();
 };
